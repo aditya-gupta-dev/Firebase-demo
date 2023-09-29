83e8ae752c00e22cad9b163ef461a5dbae92afe9
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-29 10:51:58 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    firebaseCredentials: {
+        apiKey: "AIzaSyACdTukbllVK8srusflpscaffWng3jvGg0",
+        authDomain: "daring-phoenix-397715.firebaseapp.com",
+        projectId: "daring-phoenix-397715",
+        storageBucket: "daring-phoenix-397715.appspot.com",
+        messagingSenderId: "980240483567",
+        appId: "1:980240483567:web:0b0ecfcf0422072c6a071a",
+        measurementId: "G-8JLBF83JX4"
+    }
+}
